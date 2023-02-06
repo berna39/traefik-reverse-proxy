906824e2e36f9fcddd680d46113437e43fd5813f
berna39 <kalemajoseph39@gmail.com>
2023-02-06 22:23:12 +0200

    Initial situation (3 Backend instances)

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.APPID;
+
+app.get('/', async (req, res) => {
+    res.send(`Response from the server ${port}`);
+});
+
+app.listen(port, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(`Server up and running on ${port}`);
+    }
+});
